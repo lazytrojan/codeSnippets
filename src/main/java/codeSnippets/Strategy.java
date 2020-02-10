02f4ffb6d21ec2ebba8446fe647e46d9707c2ab1
sahil-reffd <sahil@reffd.com>
2020-02-09 21:50:35 -0800

    Initial commit

---
+++ src/main/java/codeSnippets/Strategy.java
@@ -0,0 +1,69 @@
+package codeSnippets;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+interface config {
+
+}
+
+class s3Config implements config {
+
+}
+
+class MCConfig implements config {
+
+}
+
+interface DataStreamDeploymentStrategy {
+    void doAnalyse(config foo) ;
+}
+
+class S3DataStreamDeployment implements DataStreamDeploymentStrategy {
+    @Override
+    public void doAnalyse(config foo) {
+        System.out.println(foo.getClass());
+    }
+}
+
+class MCDataStreamDeployment implements DataStreamDeploymentStrategy {
+    @Override
+    public void doAnalyse(config foo) {
+        System.out.println(foo.getClass());
+    }
+}
+
+
+class DSContext {
+    private DataStreamDeploymentStrategy strategy;
+
+    public DSContext(DataStreamDeploymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void doAnalyse(config foo) {
+        strategy.doAnalyse(foo);
+    }
+}
+
+
+
+
+
+public class Strategy {
+    static Map<String, DataStreamDeploymentStrategy> registry = new HashMap<>();
+
+    static {
+        registry.put("S3", new S3DataStreamDeployment());
+        registry.put("SFMC",new  MCDataStreamDeployment());
+    }
+    public static void main(String[] args) {
+        config foo = new s3Config();
+        DSContext context = new DSContext(registry.get("S3"));
+        context.doAnalyse(foo);
+        context = new DSContext(registry.get("SFMC"));
+        foo = new MCConfig();
+        context.doAnalyse(foo);
+    }
+}
